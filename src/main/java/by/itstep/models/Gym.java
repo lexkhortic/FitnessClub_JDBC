dc4a864782c37b2238891da08fde3d6bb295669c
lexkhortic <ringostarr1997083@gmail.com>
2023-03-08 01:39:33 +0300

    New HW with JDBC

--- src/main/java/by/itstep/models/Gym.java
+++ src/main/java/by/itstep/models/Gym.java
@@ -1,8 +1,14 @@
 package by.itstep.models;
 
 public enum Gym {
-    STUDIO_A,
-    STUDIO_B,
-    STUDIO_C,
-    SWIMMING_POOL
+    STUDIO_A("Зал А"),
+    STUDIO_B("Зал Б"),
+    STUDIO_C("Зал В"),
+    SWIMMING_POOL("Бассейн");
+
+    final String typeGym;
+
+    Gym(String typeGym) {
+        this.typeGym = typeGym;
+    }
 }
